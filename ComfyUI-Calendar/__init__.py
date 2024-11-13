9281342133247863c1aa1a892aa356f2c0dad646
Rahul Y Gupta <RYG81@users.noreply.github.com>
2024-11-13 13:17:50 -0800

    Added All node code

---
+++ ComfyUI-Calendar/__init__.py
@@ -0,0 +1,28 @@
+# Import your node classes from the respective module
+# from .FashionX.fashion_nodes import Fashion
+from .Nodes.ComfyCalendarNode import (
+    ComfyCalendarNode
+)
+
+
+# Create a dictionary mapping node categories to their respective classes
+NODE_CLASS_MAPPINGS = {
+    "Comfy Calendar Node": ComfyCalendarNode
+}
+
+# Create a dictionary mapping display names to their readable names
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SportyStyleNode": "Sporty Style Node"
+}
+
+# Function to load nodes and print their status
+
+
+def load_calendar_nodes():
+    print("Calendar Nodes Loading...")
+    for display_name, readable_name in NODE_DISPLAY_NAME_MAPPINGS.items():
+        print(f"Loading: {readable_name} as {display_name}")
+
+
+# Execute the function to load nodes and confirm their status
+load_calendar_nodes()
